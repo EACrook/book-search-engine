af87e41f2e57ca8e5400e19bff58660c0189196f
Emily Crook <Emily.A.Crook96@gmail.com>
2021-07-17 16:48:46 -0500

    signup functionality working

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client"
+
+export const SIGN_UP = gql`
+    mutation signUp($username: String!, $email: String!, $password: String!){
+        signUp(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
